eade4c83e9fd9c629b1e339127cd1cc2f986714c
english <english@iMac-de-admin.local>
2019-10-30 14:27:38 -0400

    Initial Commit

---
+++ image_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  image_app
+//
+//  Created by english on 2019-10-30.
+//  Copyright © 2019 ajax. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
